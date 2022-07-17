790c0a418a3d91ba4d532d8e4ac68ac67ec4653f
Unique <oladejomubarakade@gmail.com>
2022-07-17 07:17:42 +0100

    Java seven segment

---
+++ SevenSegmentsDisplay.java
@@ -0,0 +1,9 @@
+package sevenSegment;
+
+public class SevenSegmentsDisplay {
+    public static void main(String[] args) {
+        SevenSegments.setScreen("11100001");
+        SevenSegments.display();
+
+    }
+}
